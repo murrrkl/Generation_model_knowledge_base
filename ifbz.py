aec4be9d7da9cd44b1197bb2d14f5974d0365021
murrrkl <45383340+murrrkl@users.noreply.github.com>
2022-12-02 12:36:01 +1000

    Update ifbz.py

--- ifbz.py
+++ ifbz.py
@@ -7,6 +7,8 @@ import random
 from   random import randint
 
 w = 1
+COUNT_CLASS = 2
+COUNT_FEATURES = 2
 
 class Table(Frame):
     def __init__(self, parent=None, headings=tuple(), rows=tuple()):
@@ -17,12 +19,8 @@ class Table(Frame):
         table["displaycolumns"] = headings
 
         global w
-        i = 0
         for head in headings:
-            if i > 0 and w != 30 and w != 100:
-                w = 60
 
-            i += 1
             table.heading(head, text=head, anchor=CENTER)
             table.column(head, width=w, anchor=CENTER)
 
@@ -733,27 +731,29 @@ def merge_one_periods(count_ib, count_class, count_features):
 
             class_name = "class" + str(i)
             feature_name = "feature" + str(j)
-            val.sort()
+            val = list(set(val))
+            val = sorted(val)
 
             cur1.execute('INSERT INTO merge_(name_class, name_feature, count_periods, ng, vg_per, values_per) VALUES( ?, ?, ?, ?, ?, ?)',
-                (class_name, feature_name, 1, ng, vg, str(list(set(val))).strip('[]')))
+                (class_name, feature_name, 1, ng, vg, str(val).strip('[]')))
 
     conn1.commit()
 
-def merge_two(class_name, feature_name, i, n1,v1, va1, n2, v2, va2):
+def two(class_name, feature_name, i, n1,v1, va1, n2, v2, va2):
 
     conn1 = sqlite3.connect('ifbz.db')
     cur1 = conn1.cursor()
 
-    id = i
-    ng1 = n1
-    vg1 = v1
-    val1 = va1
-    ng2 = n2
-    vg2 = v2
-    val2 = va2
+    id = i.copy()
+    ng1 = n1.copy()
+    vg1 = v1.copy()
+    val1 = va1.copy()
+    ng2 = n2.copy()
+    vg2 = v2.copy()
+    val2 = va2.copy()
 
     merge_id = []
+    merge_id.append(id[0])
     merge_ng1 = []
     merge_vg1 = []
     merge_val1 = []
@@ -761,100 +761,127 @@ def merge_two(class_name, feature_name, i, n1,v1, va1, n2, v2, va2):
     merge_vg2 = []
     merge_val2 = []
 
-    flag = True
 
-    while flag == True:
+    count = 0
 
-        merge_id = []
-        merge_ng1 = []
-        merge_vg1 = []
-        merge_val1 = []
-        merge_ng2 = []
-        merge_vg2 = []
-        merge_val2 = []
+
+
+    for i in range(0, len(id)):
+
+        if id[i] == merge_id[0]:
+            merge_ng1.append(ng1[i])
+            merge_vg1.append(vg1[i])
+            merge_val1.append(val1[i])
+            merge_ng2.append(ng2[i])
+            merge_vg2.append(vg2[i])
+            merge_val2.append(val2[i])
+        else:
+            count = i
+            break
+
+    if len(id) != 1:
+        flag = True
+    else:
         flag = False
-        
+    #print(merge_val1)
+    #print(merge_val2)
 
-        for i in range(len(id) - 1):
-            for j in range(i+1, len(id)):
-                if id[i] + 1 == id[j]:
-                    v1 = val1[i]
+
+    while flag:
+
+        flag = False
+        m_ng1 = []
+        m_vg1 = []
+        m_val1 = []
+        m_ng2 = []
+        m_vg2 = []
+        m_val2 = []
+        index = 0
+        n = 0
+
+        for i in range(0, len(merge_val1)):
+            for j in range(count, len(id)):
+                if j == count:
+                    index = id[j]
+                if id[j] == index:
+
+                    v1 = merge_val1[i].copy()
                     v1.extend(val1[j])
-                    v2 = val2[i]
+                    v2 = merge_val2[i].copy()
                     v2.extend(val2[j])
+
+                    #print(merge_val1)
+                    #print(merge_val2)
+                    '''
+                    print("id = ",id[j])
+                    print("1-1 = ",merge_vg1[i])
+                    print("1.2 ", vg1[j])
+                    print("2.1 ", merge_vg2[i])
+                    print("2.2 ", vg2[j])
+                    '''
+
+
+                    v1 = list(set(v1))
+                    v2 = list(set(v2))
+                    v1.sort()
+                    v2.sort()
+                    #print(v1)
+                    #print(v2)
+
                     sovp = [x for x in v1 if x in v2]
 
+
                     if sovp == []:
-                        merge_id.append(id[j])
-                        merge_ng1.append(min(ng1[i], ng1[j]))
-                        merge_vg1.append(max(vg1[i], vg1[j]))
-                        merge_val1.append(v1)
-                        merge_ng2.append(min(ng2[i], ng2[j]))
-                        merge_vg2.append(max(vg2[i], vg2[j]))
-                        merge_val2.append(v2)
+                        m_ng1.append(min(merge_ng1[i], ng1[j]))
+                        m_vg1.append(max(merge_vg1[i], vg1[j]))
+                        m_val1.append(v1)
+                        m_ng2.append(min(merge_ng2[i], ng2[j]))
+                        m_vg2.append(max(merge_vg2[i], vg2[j]))
+                        m_val2.append(v2)
+                        n = j + 1
                         flag = True
-            if flag == True:
-                id = merge_id
-                ng1 = merge_ng1
-                vg1 = merge_vg1
-                val1 = merge_val1
-                ng2 = merge_ng2
-                vg2 = merge_vg2
-                val2 = merge_val2
+                    #print("-----")
+                else:
+                    n = j
+                    break
 
+        count = n
+        if flag == True:
+            merge_ng1 = m_ng1
+            merge_vg1 = m_vg1
+            merge_val1 = m_val1
+            merge_ng2 = m_ng2
+            merge_vg2 = m_vg2
+            merge_val2 = m_val2
 
-    ng1 = ng1[0]
-    vg1 = vg1[0]
-    val1 = val1[0]
-    ng2 = ng2[0]
-    vg2 = vg2[0]
-    val2 = val2[0]
+    if len(id) != 1:
+        ng1 = merge_ng1[0]
+        vg1 = merge_vg1[0]
+        val1 = merge_val1[0]
+        ng2 = merge_ng2[0]
+        vg2 = merge_vg2[0]
+        val2 = merge_val2[0]
 
-    if len(merge_id) >= 1:
+        val1 = list(set(val1))
+        val2 = list(set(val2))
+        val1 = sorted(val1)
+        val2 = sorted(val2)
 
-        for i in range(1, len(merge_id)):
-            v1 = val1[0]
-            v1.extend(merge_val1[i])
-            v2 = val2[0]
-            v2.extend(merge_val2[i])
-            sovp = [x for x in v1 if x in v2]
+        sovp = [x for x in val1 if x in val2]
 
-            if sovp == []:
-                ng1 = min(ng1, merge_ng1[i])
-                vg1 = max(vg1, merge_vg1[i])
-                val1 = v1
-                ng2 = min(ng2, merge_ng2[i])
-                val2 = v2
-                vg2 = max(vg2, merge_vg2[i])
+        if sovp != []:
+            id = []
+        else:
+            id = [1]
 
-
-    val1.sort()
-    val2.sort()
-
-    sovp = [x for x in val1 if x in val2]
-
-    if sovp != []:
-        id = []
-
-    if len(id) != 0:
-        cur1.execute(
-            'INSERT INTO merge_(name_class, name_feature, count_periods, ng, vg_per, values_per) VALUES( ?, ?, ?, ?, ?, ?)',
-            (class_name, feature_name, 1, ng1, vg1, str(list(set(val1))).strip('[]')))
-        cur1.execute(
-            'INSERT INTO merge_(name_class, name_feature, count_periods, ng, vg_per, values_per) VALUES( ?, ?, ?, ?, ?, ?)',
-            (class_name, feature_name, 2, ng2, vg2, str(list(set(val2))).strip('[]')))
-        conn1.commit()
-
-    print("НГ1 = ", ng1)
-    print("ВГ1 = ", vg1)
-    print("ЗДП1 = ", str(list(set(val1))).strip('[]'))
-    print("НГ2 = ", ng2)
-    print("ВГ2 = ", vg2)
-    print("ЗДП2 = ", str(list(set(val2))).strip('[]'))
-
-
-
-
+        if len(id) != 0:
+            cur1.execute(
+                'INSERT INTO merge_(name_class, name_feature, count_periods,values_per, ng, vg_per) VALUES( ?, ?, ?, ?, ?, ?)',
+                (class_name, feature_name, 1, str(val1).strip('[]'), ng1, vg1 ))
+            cur1.execute(
+                'INSERT INTO merge_(name_class, name_feature, count_periods,values_per, ng, vg_per) VALUES( ?, ?, ?, ?, ?, ?)',
+                (class_name, feature_name, 2, str(val2).strip('[]'),  ng2, vg2))
+            conn1.commit()
 
 
 def merge_two_periods(count_ib, count_class, count_features):
@@ -880,6 +907,7 @@ def merge_two_periods(count_ib, count_class, count_features):
             ng2 = []
             vg2 = []
             val2 = []
+            count = 0
             for row in rows:
                 if row[1] == "class" + str(i) and row[2] == "feature" + str(j) and row[3] == 2:
                     if count_per == 1:
@@ -894,17 +922,16 @@ def merge_two_periods(count_ib, count_class, count_features):
                         val2.append(literal_eval("[" + row[6] + "]"))
                         count_per = 1
 
-            if id != []:
-                print("Класс = ", class_name)
-                print("Признак = ", feature_name)
-                #print("id = ", id)
-                #print("НГ1 = ", ng1)
-                #print("ВГ1 = ", vg1)
-                #print(val1)
-                #print("НГ2 = ", ng2)
-                #print("ВГ2 = ", vg2)
-                #print(val2)
-                merge_two("class" + str(i),  "feature" + str(j), id, ng1, vg1, val1, ng2, vg2, val2)
+            for k in range(1, len(id)):
+                if id[k] != id[k-1]:
+                    count += 1
+
+
+
+            if id != [] and count > (count_ib / count_class) // 2:
+                #print("Класс = ", class_name)
+                #print("Признак = ", feature_name)
+                two("class" + str(i), "feature" + str(j), id, ng1, vg1, val1, ng2, vg2, val2)
 
 
 
@@ -915,6 +942,8 @@ def merge_two_periods(count_ib, count_class, count_features):
 def merge():
     global conn
     global cur
+    global COUNT_CLASS
+    global COUNT_FEATURES
 
     cur = conn.execute('SELECT * FROM borders')
     rows = cur.fetchall()
@@ -936,9 +965,9 @@ def merge():
         if row[0] > count_ib:
             count_ib += 1
 
-    print("Кол-во историй болезней:", count_ib)
-    print("Кол-во классов:", count_class)
-    print("Кол-во признаков:", count_features)
+
+    COUNT_CLASS = count_class
+    COUNT_FEATURES = count_features
 
     merge_one_periods(count_ib, count_class, count_features)
     merge_two_periods(count_ib, count_class, count_features)
@@ -947,9 +976,103 @@ def merge():
     data = (row for row in cur.fetchall())
 
     global w
-    w = 100
-    table = Table(root, headings=('Класс', 'Признак', '№ периода', 'НГ', 'ВГ', 'ЗДП'), rows=data)
-    table.grid(row=4, column=0, columnspan=5, pady=5)
+    w = 120
+    table = Table(root, headings=('Класс', 'Признак', '№ периода', 'ЗДП', 'НГ', 'ВГ'), rows=data)
+    table.grid(row=4, column=0, columnspan=7, pady=5)
+
+def create_final():
+    global conn
+    global cur
+    global COUNT_CLASS
+    global COUNT_FEATURES
+
+    cur = conn.execute('SELECT * FROM merge_')
+    rows = cur.fetchall()
+
+    cl = []
+    pr = []
+
+
+    for i in range(1, COUNT_CLASS + 1):
+        for j in range(1, COUNT_FEATURES + 1):
+            cl.append(i)
+            pr.append(j)
+
+
+    for i in range(0, len(cl)):
+        ng1 = 0
+        vg1 = 0
+        val1 = []
+        ng2 = 0
+        vg2 = 0
+        val2 = []
+        ng3 = 0
+        vg3 = 0
+        val3 = []
+        ng4 = 0
+        vg4 = 0
+        val4 = []
+        ng5 = 0
+        vg5 = 0
+        val5 = []
+
+        for row in rows:
+            if row[0] == "class" + str(cl[i]) and row[1] == "feature" + str(pr[i]):
+                if row[2] == 1:
+                    val1 = row[3]
+                    ng1 = row[4]
+                    vg1 = row[5]
+                elif row[2] == 2:
+                    val2 = row[3]
+                    ng2 = row[4]
+                    vg2 = row[5]
+                elif row[2] == 3:
+                    val3 = row[3]
+                    ng3 = row[4]
+                    vg3 = row[5]
+                elif row[2] == 4:
+                    val4 = row[3]
+                    ng4 = row[4]
+                    vg4 = row[5]
+                else:
+                    val5 = row[3]
+                    ng5 = row[4]
+                    vg5 = row[5]
+
+
+        cur.execute(
+            'INSERT INTO final (name_class, name_feature, count_periods,values_per, ng, vg_per) VALUES( ?, ?, ?, ?, ?, ?)',
+            ("class" + str(cl[i]), "feature" + str(pr[i]), 1, str(val1), ng1, vg1))
+
+        if vg2 != 0:
+            cur.execute('INSERT INTO final (name_class, name_feature, count_periods,values_per, ng, vg_per) VALUES( ?, ?, ?, ?, ?, ?)',
+            ("class" + str(cl[i]), "feature" + str(pr[i]), 2, str(val2), ng2, vg2))
+
+
+        if vg3 != 0:
+            cur.execute(
+                'INSERT INTO final (name_class, name_feature, count_periods,values_per, ng, vg_per) VALUES( ?, ?, ?, ?, ?, ?)',
+                ("class" + str(cl[i]), "feature" + str(pr[i]), 3, str(val3), ng3, vg3))
+
+        if vg4 != 0:
+            cur.execute(
+                'INSERT INTO final (name_class, name_feature, count_periods,values_per, ng, vg_per) VALUES( ?, ?, ?, ?, ?, ?)',
+                ("class" + str(cl[i]), "feature" + str(pr[i]), 4, str(val4), ng4, vg4))
+
+        if vg5 != 0:
+            cur.execute('INSERT INTO final (name_class, name_feature, count_periods,values_per, ng, vg_per) VALUES( ?, ?, ?, ?, ?, ?)',
+            ("class" + str(cl[i]), "feature" + str(pr[i]), 5, str(val5), ng5, vg5))
+
+        conn.commit()
+
+    cur.execute("SELECT * FROM final")
+    data = (row for row in cur.fetchall())
+
+    global w
+    w = 120
+    table = Table(root, headings=('Класс', 'Признак', '№ периода', 'ЗДП', 'НГ', 'ВГ'), rows=data)
+    table.grid(row=6, column=0, columnspan=7, pady=5)
+
 
 
 def click_FIBZ():
@@ -971,10 +1094,16 @@ def click_FIBZ():
     # Удаление и повторное создание ЧПД признаков
     cur.execute('DROP table if exists merge_')
     cur.execute(
-        "CREATE TABLE merge_ (name_class text, name_feature text, count_periods integer, ng integer, vg_per, values_per text)")
+        "CREATE TABLE merge_ (name_class text, name_feature text, count_periods integer, values_per text, ng integer, vg_per)")
+
+    # Удаление и повторное создание ЧПД признаков
+    cur.execute('DROP table if exists final')
+    cur.execute(
+        "CREATE TABLE final (name_class text, name_feature text, count_periods integer, values_per text, ng integer, vg_per)")
 
     Borders()
     merge()
+    create_final()
 
 
 b_fibz = Button(root, text = "Сформировать", bg = "white", command = click_FIBZ)
